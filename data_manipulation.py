c3f529c9418d516fae792f24eff6598fd3b69a5f
mfbult1217@ung.edu <laxrules227>
2018-07-25 20:01:29 -0400

    Data manipulation file using: conversion to JSON pandas Output graphs from matplotlib

---
+++ data_manipulation.py
@@ -0,0 +1,46 @@
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
+"""Appends data to json format"""
+
+tweets_data_path = 'C:\Users\mfbult1217\PycharmProjects\tweet_miner\output.txt'
+tweets_data = []
+tweets_file = open(tweets_data_path, "r")
+for line in tweets.file:
+    try:
+        tweet = json.loads(line)
+        tweets_data.append(tweet)
+    except:
+        continue
+
+"""Filtering and organizing tweets by country and number of tweets"""
+
+tweets = pd.DataFrame()
+tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
+tweets['lang'] = map(lambda tweet: tweet['lang'], tweets_data)
+tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, tweets_data)
+
+
+tweets_by_lang = tweets['lang'].value_counts()
+
+fig, ax = plt.subplots()
+ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.set_xlabel('languages', fontsize=15)
+ax.set_ylabel('Number of tweets', fontsize=15)
+ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
+tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
+
+
+tweets_by_country = tweets['country'].value_counts()
+
+fig, ax = plt.subplots()
+ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.set_xlabel('Countries', fontsize=15)
+ax.set_ylabel('Number of tweets', fontsize=15)
+ax.set_title('top 5 countries', fontsize=15, fontweight='bold')
+tweets_by_country[:5].plot(ax=ax. kind='bar', color='blue')
+
+
